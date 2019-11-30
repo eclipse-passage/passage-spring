f218c79baf4bc14ef9551181f9f5aefb693efc48
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-30 16:00:53 +0300

    553631 init project structure  - inplace `lic.api` dependency until #553631 is fixed
    Signed-off-by: elena.parovyshnaya <elena.parovyshnaya@gmail.com>

--- incoming/org.eclipse.passage.lic.api/build.gradle.kts
+++ incoming/org.eclipse.passage.lic.api/build.gradle.kts
@@ -1,8 +1,18 @@
-
+/*******************************************************************************
+ * Copyright (c) 2019 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
 plugins {
     java
 }
-
 
 repositories {
     jcenter()
@@ -10,4 +20,3 @@ repositories {
 
 dependencies {
 }
-
